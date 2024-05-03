42d1ecf1cd7130b540ca8cafdc02215da499fa3b
Kentaro Furukawa <info@ken89.com>
2024-05-04 06:39:01 +0900

    feat: offline support
    some offline support..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
+import serviceWorker from "astrojs-service-worker";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: [partytown(), serviceWorker()]
 });
